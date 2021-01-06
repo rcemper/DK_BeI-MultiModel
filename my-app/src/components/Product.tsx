9a79c2fb5f24613466ee0b3d74b735d062c27a0b
bert sarens <sarensbert@gmail.com>
2021-01-06 21:06:40 +0100

    gitignore eslint cache + product image

--- my-app/src/components/Product.tsx
+++ my-app/src/components/Product.tsx
@@ -7,7 +7,7 @@ interface IProductProps {
 export function Product({ product }: IProductProps ) {
     return (
         <div className="card col">
-          <img src="..." className="card-img-top" alt="..."/>
+          <img src="logo512.png" className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
             <p className="card-text">The product id is: {product.id}</p>
