c9a62cb6eedd3b933b09370aeb43ddb1037ee110
bert sarens <sarensbert@gmail.com>
2021-01-10 20:18:05 +0100

    added price to products

--- my-app/src/mocks/products.ts
+++ my-app/src/mocks/products.ts
@@ -4,21 +4,29 @@ export const products: IProduct[] = [
     { 
         "id" : 1,
         "category_id" : 1,
-        "name": "product 1"
+        "category_name" : "cat 1",
+        "name": "product 1",
+        "price": 10
       },
       { 
         "id" : 2,
         "category_id" : 1,
-        "name": "product 21"
+        "category_name" : "cat 1",
+        "name": "product 21",
+        "price": 10
       },
       { 
         "id" : 3,
         "category_id" : 2,
-        "name": "product 3"
+        "category_name" : "cat 2",
+        "name": "product 3",
+        "price": 10
       },
       { 
         "id" : 4,
         "category_id" : 3,
-        "name": "product 4"
+        "category_name" : "cat 3",
+        "name": "product 4",
+        "price": 10
       }
 ];
