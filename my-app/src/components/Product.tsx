3a63675c16c01c0372c09dc6008e3e820665bf21
bert sarens <sarensbert@gmail.com>
2021-01-12 19:44:57 +0100

    added attribute types, fixed a bug with 0 as last id

--- my-app/src/components/Product.tsx
+++ my-app/src/components/Product.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IProduct } from "../types";
+import { doesProductNameContain } from "../utils";
 
 interface IProductProps {
     product: IProduct;
@@ -11,6 +12,11 @@ export function Product({ product }: IProductProps ) {
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
             <p className="card-text">The product id is: {product.id}</p>
+            <ul>
+              {product.attributes?.map((attribute) => (
+                <li key={attribute.id}>{attribute.attribute} : {attribute.name}</li>
+              ))}
+            </ul>
           </div>
         </div>
     );
---
+++ my-app/src/types/attribute.ts
@@ -0,0 +1,6 @@
+export interface IAttribute {
+    attribute: string;
+    name: string;
+    value: string;
+    id: number;
+}
